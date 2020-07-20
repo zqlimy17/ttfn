94cd16026af298b9dfb3d3ec5a51f299fd7ab46e
zqlimy17 <zqlimy@gmail.com>
2020-07-21 02:43:04 +0800

    result display

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -1,20 +1,52 @@
 import React, { FC, useEffect } from "react";
+import { Container } from "@material-ui/core";
+
+const styles: any = {
+    root: {
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
+    },
+    flex: {
+        flexGrow: "1",
+        flexBasis: "22%",
+        minWidth: "200px",
+        margin: "0.5em",
+    },
+    imageContainer: {
+        position: "relative",
+        paddingBottom: "67%",
+    },
+    image: {
+        objectFit: "cover",
+        width: "100%",
+        height: "100%",
+        position: "absolute",
+    },
+};
 
 interface ResultsProps {
     results: any[];
 }
 
 const Results: FC<ResultsProps> = ({ results }) => {
+    const classes = styles;
     useEffect(() => {
         console.log(results);
     }, [results]);
     return (
-        <div>
+        <Container style={classes.root} maxWidth={"xl"}>
             {results.map((result, index) => (
-                // <div key={index}>{index}</div>
-                <img src={result.images.original_still.url} />
+                <div style={classes.flex} key={index}>
+                    <div style={classes.imageContainer}>
+                        <img
+                            src={result.images.original_still.url}
+                            style={classes.image}
+                        />
+                    </div>
+                </div>
             ))}
-        </div>
+        </Container>
     );
 };
 
