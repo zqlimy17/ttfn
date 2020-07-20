5cb39028686a954dab2f153a5d465d1316df0b91
zqlimy17 <zqlimy@gmail.com>
2020-07-20 17:33:22 +0800

    init components

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Nav: FC = () => {
+    return <div></div>;
+};
+
+export default Nav;
---
+++ src/components/Results/Results.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Results: FC = () => {
+    return <div></div>;
+};
+
+export default Results;
