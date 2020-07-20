cd472ade84499114432eb6489a958beb7018c83f
zqlimy17 <zqlimy@gmail.com>
2020-07-21 04:17:38 +0800

    like button

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState, useEffect } from "react";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import FavoriteTwoToneIcon from "@material-ui/icons/FavoriteTwoTone";
 
@@ -8,10 +8,20 @@ const styles: any = {
         paddingBottom: "67%",
     },
     image: {
+        cursor: "pointer",
         objectFit: "cover",
         width: "100%",
         height: "100%",
         position: "absolute",
+    },
+    like: {
+        zIndex: 2,
+        position: "absolute",
+        bottom: 0,
+        right: 0,
+        color: "red",
+        padding: "5px",
+        fontSize: "48px",
     },
 };
 
@@ -23,7 +33,18 @@ interface ImageProps {
 
 const Image: FC<ImageProps> = ({ result, favourites, setFavourites }) => {
     const classes = styles;
+    const [hover, setHover] = useState<boolean>(false);
+    const [liked, setLiked] = useState<boolean>(false);
 
+    useEffect(() => {
+        try {
+            if (favourites.includes(result.images.original_still.url)) {
+                setLiked(true);
+            }
+        } catch (error) {
+            alert(error.message);
+        }
+    }, [favourites]);
     return (
         <div
             style={classes.imageContainer}
@@ -33,8 +54,12 @@ const Image: FC<ImageProps> = ({ result, favourites, setFavourites }) => {
                     result.images.original_still.url,
                 ])
             }
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
         >
             <img src={result.images.original_still.url} style={classes.image} />
+            {hover ? <FavoriteTwoToneIcon style={classes.like} /> : ""}
+            {liked ? <FavoriteIcon style={classes.like} /> : ""}
         </div>
     );
 };
